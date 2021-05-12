0c2912f746c02882157eeec5e97c4d366ba7f776
Haylee <17357481+hailstormCodes@users.noreply.github.com>
2021-05-11 23:28:05 -0400

    Added Collections Page and moved collections data into redux

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>COLLECTION PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { Route } from 'react-router-dom';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({ match }) => 
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component= {CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CollectionPage} /> 
+  </div>;
 
-const ShopPage = ({ collections }) => (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-);
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
