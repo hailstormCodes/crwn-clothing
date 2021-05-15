e3aa24f29d28172fdfb93437b19a9daa5d2c3748
Haylee <17357481+hailstormCodes@users.noreply.github.com>
2021-05-15 15:08:00 -0400

    Updated collections page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from "./firebase/firebase.utils";
+
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
